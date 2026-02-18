aa668aa3313abf24498e8e52071690eef8a4e016
Kei <maxwell.kei.farouk@gmail.com>
2026-02-18 15:41:33 +0200

    impliment slower methods

--- 12practical/src/Main.java
+++ 12practical/src/Main.java
@@ -6,29 +6,24 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main{
+
+    static int countOn3 = 0;
+    static int countOn2A = 0;
+    static int countOn2B = 0;
+    static int countOn = 0;
+
     public static void main(String[] args)
     {
-        int[] arr = createArray(10);
+
+        int[] arrSizes = {10, 100, 1000};
+
+        int[] arr = createArray(20);
         System.out.println(Arrays.toString(arr));
-        System.out.println(mscOn(arr));
-    }
+        System.out.println("Kadanes: " + mscOn(arr) + "steps: " + countOn);
+        System.out.println("O(n^3): " + mcsOn3(arr) + "steps: " + countOn3);
+        System.out.println("O(n^2)(A): " + mcsOn2A(arr) + "steps: " + countOn2A);
+        System.out.println("O(n^2)(B): " + mcsOn2B(arr) + "steps: " + countOn2B);
 
-    public static int mscOn(int[] arr)
-    {
-        int N = arr.length;
-        double maxSoFar = 0.0;
-        double maxToHere = 0.0;
-
-        for (int j : arr) {
-            if (j + maxToHere > 0) {
-                maxToHere += j;
-            } else {
-                maxToHere = 0;
-            }
-            if (maxToHere > maxSoFar) maxSoFar = maxToHere;
-        }
-        if (maxSoFar == 0) return 0;
-        else return (int)maxSoFar;
     }
 
     public static int[] createArray(int n)
@@ -43,4 +38,88 @@ public class Main{
 
         return arr;
     }
+
+    public static int mcsOn3(int[] arr)
+    {
+        int N = arr.length;
+        int maxSoFar = 0;
+
+        for (int low = 0; low < N; low++)
+        {
+            for (int high = low; high < N; high++)
+            {
+                int sum = 0;
+                for (int r = low; r <= high; r++) {
+                    sum += arr[r];
+                    countOn3++;
+                }
+                if (sum > maxSoFar)
+                {
+                    maxSoFar = sum;
+                }
+            }
+        }
+        return maxSoFar;
+    }
+
+    public static int mcsOn2A(int[] arr) {
+        int N = arr.length;
+        int maxSoFar = 0;
+
+        for (int low = 0; low < N; low++) {
+            int sum = 0;
+            for (int r = low; r < N; r++) {
+                sum += arr[r];
+                countOn2A++;
+                if (sum > maxSoFar) {
+                    maxSoFar = sum;
+                }
+            }
+        }
+        return maxSoFar;
+    }
+
+    public static int mcsOn2B(int[] arr) {
+        int N = arr.length;
+        int[] sumTo = new int[N + 1];
+        sumTo[0] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sumTo[i] = sumTo[i - 1] + arr[i - 1];
+        }
+        int maxSoFar = 0;
+        for (int low = 0; low < N; low++) {
+            for (int high = low; high < N; high++) {
+                int sum = sumTo[high + 1] - sumTo[low];
+                countOn2B++;
+                if (sum > maxSoFar) {
+                    maxSoFar = sum;
+                }
+            }
+        }
+        return maxSoFar;
+    }
+
+
+    public static int mscOn(int[] arr)
+    {
+        int N = arr.length;
+        double maxSoFar = 0.0;
+        double maxToHere = 0.0;
+
+        for (int j : arr) {
+            if (j + maxToHere > 0) {
+                maxToHere += j;
+            } else {
+                maxToHere = 0;
+            }
+            countOn++;
+            if (maxToHere > maxSoFar) maxSoFar = maxToHere;
+        }
+        if (maxSoFar == 0) return 0;
+        else return (int)maxSoFar;
+    }
+
+
+
 }
