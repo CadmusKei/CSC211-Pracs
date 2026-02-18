d5207c1b507134dd917eb6aa3e586bf3f2ac6ddf
Kei <maxwell.kei.farouk@gmail.com>
2026-02-18 14:50:12 +0200

    impliment mcsOn method

--- 12practical/src/Main.java
+++ 12practical/src/Main.java
@@ -8,20 +8,42 @@ import java.util.Random;
 public class Main{
     public static void main(String[] args)
     {
-        int[] arr = createArray(30);
+        int[] arr = createArray(5);
         System.out.println(Arrays.toString(arr));
+        System.out.println(mscOn(arr));
     }
 
     public static int[] createArray(int n)
     {
         int[] arr = new int[n];
-        Random randGenartor = new Random();
+        Random randGen = new Random();
 
         for (int i = 0; i < n; i++)
         {
-             arr[i] = (int)(randGenartor.nextInt(1, n)* Math.pow((-1), randGenartor.nextInt(2, 4)));
+             arr[i] = (int)(randGen.nextInt(1, n)* Math.pow((-1), randGen.nextInt(2, 4)));
         }
 
         return arr;
     }
+
+    public static int mscOn(int[] arr)
+    {
+        int N = arr.length;
+        double maxSoFar = 0.0;
+        double maxToHere = 0.0;
+
+        for (int j : arr) {
+            if (j + maxToHere > 0) {
+                maxToHere += j;
+            } else {
+                maxToHere = 0;
+            }
+            if (maxToHere > maxSoFar) maxSoFar = maxToHere;
+        }
+        if (maxSoFar == 0) return 0;
+        else return (int)maxSoFar;
+    }
+
+
+
 }
