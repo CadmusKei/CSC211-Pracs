f034eb64b852d066c5b8d98f3a2c30a7d2cbb761
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-03-06 10:40:59 +0200

    Add other practicals

--- MCS/src/Main.java
+++ MCS/src/Main.java
@@ -1,45 +1,147 @@
- public class Main{
+// 4502894
+// Maxwell Kei Farouk
+// Wednesday E1/E2
+
+import java.util.Random;
+
+// Main Class
+
+public class Main{
+
+    // count variables
+    static int countOn3 = 0;
+    static int countOn2A = 0;
+    static int countOn2B = 0;
+    static int countOn = 0;
+
+    // Reset the counters
+    public static void resetCounters() {
+        countOn3 = 0;
+        countOn2A = 0;
+        countOn2B = 0;
+        countOn = 0;
+    }
+
+    // Main executable method
     public static void main(String[] args)
     {
-        int[] arr = {-4, 3, 5, -3, -5, 8, 1};
+        int[] sizes = {10, 100, 1000, 3000};
 
-    }
+        System.out.printf("%-8s %-15s %-15s %-15s %-15s%n",
+                "n", "O(n^3)", "O(n^2)A", "O(n^2)B", "O(n)");
 
-    static void on2Method(int[] arr)
-    {
-        int n = arr.length;
-        int[] sumTo = new int[n+1];
+        for (int n : sizes) {
 
-        for (int i = 0; i < n; i++)
-        {
-            sumTo[i] = sumTo[i-1] + arr[i];
-        }
-        int maxSoFar = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = n; j <= 0; j-- )
-            {
-                return;
-            }
+            int[] X = createArray(n);
+            resetCounters();
+
+            mcsOn3(X);
+            mcsOn2A(X);
+            mcsOn2B(X);
+            mcsOn(X);
+
+            System.out.printf("%-8d %-15d %-15d %-15d %-15d%n",
+                    n, countOn3, countOn2A, countOn2B, countOn);
         }
     }
 
-    static void onMethod(int[] arr)
+    // Create the array
+    public static int[] createArray(int n)
     {
-        int maxToHere = 0;
+        int[] arr = new int[n];
+        Random randGen = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+             arr[i] = (int)(randGen.nextInt(1, n)* Math.pow((-1), randGen.nextInt(2, 4)));
+        }
+
+        return arr;
+    }
+
+    // O(n^3) Sort
+    public static int mcsOn3(int[] arr)
+    {
+        int N = arr.length;
         int maxSoFar = 0;
 
-        for (int i = 0; i < arr.length; i++)
+        for (int low = 0; low < N; low++)
         {
-            if (arr[i] + maxToHere > 0)
+            for (int high = low; high < N; high++)
             {
-                maxToHere += arr[i];
-                System.out.println(maxToHere);
+                int sum = 0;
+                for (int r = low; r <= high; r++) {
+                    sum += arr[r];
+                    countOn3++;
+                }
+                if (sum > maxSoFar)
+                {
+                    maxSoFar = sum;
+                }
             }
-            else {maxToHere = 0;}
+        }
+        if (maxSoFar < 0) return 0;
+        return maxSoFar;
+    }
+
+    // O(n^2)(A) Sort
+    public static int mcsOn2A(int[] arr) {
+        int N = arr.length;
+        int maxSoFar = 0;
+
+        for (int low = 0; low < N; low++) {
+            int sum = 0;
+            for (int r = low; r < N; r++) {
+                sum += arr[r];
+                countOn2A++;
+                if (sum > maxSoFar) {
+                    maxSoFar = sum;
+                }
+            }
+        }
+        return maxSoFar;
+    }
+
+    // O(n^2)(B) Sort
+    public static int mcsOn2B(int[] arr) {
+        int N = arr.length;
+        int[] sumTo = new int[N + 1];
+        sumTo[0] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sumTo[i] = sumTo[i - 1] + arr[i - 1];
+        }
+        int maxSoFar = 0;
+        for (int low = 0; low < N; low++) {
+            for (int high = low; high < N; high++) {
+                int sum = sumTo[high + 1] - sumTo[low];
+                countOn2B++;
+                if (sum > maxSoFar) {
+                    maxSoFar = sum;
+                }
+            }
+        }
+        return maxSoFar;
+    }
+
+    // O(n) Sort
+    public static int mcsOn(int[] arr)
+    {
+        int N = arr.length;
+        double maxSoFar = 0.0;
+        double maxToHere = 0.0;
+
+        for (int j : arr) {
+            if (j + maxToHere > 0) {
+                maxToHere += j;
+            } else {
+                maxToHere = 0;
+            }
+            countOn++;
             if (maxToHere > maxSoFar) maxSoFar = maxToHere;
         }
-
-        System.out.println("Max is: " + maxSoFar);
+        if (maxSoFar == 0) return 0;
+        else return (int)maxSoFar;
     }
- }
+
+}
